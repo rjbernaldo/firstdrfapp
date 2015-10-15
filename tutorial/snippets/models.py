b24596b3be3a5774272cb8595a736482053fedfd
Rj Bernaldo <rj.bernaldo@gmail.com>
2015-10-15 10:18:15 +0800

    Modify whitespace

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -15,5 +15,6 @@ class Snippet(models.Model):
   language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
   style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
+
   class Meta:
     ordering = ('created',)
